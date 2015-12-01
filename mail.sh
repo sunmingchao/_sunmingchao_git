8448d74a09981cc87ed874f3529807253f00b251
root <root@zbxserver.(none)>
2015-12-01 21:41:48 +0800

    增加无验证发送邮件

--- mail.sh
+++ mail.sh
@@ -3,6 +3,8 @@
 [[ $# -ne 3 ]] && echo "Usag: sh $0 user subject content " && exit
 #echo -e "`date` \nTO:\n $1 \n\nSUBJECT:\n $2 \n\nDATA:\n $3 \n=================\n"  >>/usr/local/zabbix-server/zabbix_alert.log
 
+#send email without authentication
+#/usr/local/bin/sendEmail -t "$1" -u "$2" -m "$3" -f alert@r.com -o message-charset=utf-8 -o message-content-type=html
 
 /usr/local/bin/sendEmail -s smtp.139.com -xp xxxpasswd -xu sunmch -f ssss@139.com -m "$3" -t "$1" -u "$2" -o message-charset=utf-8 -o message-content-type=html
 
